599e16da45b2e6e3cf2029342157b2cb3de2c6bc
LilRind <1337476737@qq.com>
2024-05-14 00:09:01 +0800

    根据Id查询用户和地址列表(List) DB静态工具，避免循环依赖的问题，这是mp框架所持有的。当然spring也存在依赖注入和三级工厂等方式，

--- day01-MybatisPlus/Test/mp-demo/src/main/java/com/itheima/mp/service/IUserService.java
+++ day01-MybatisPlus/Test/mp-demo/src/main/java/com/itheima/mp/service/IUserService.java
@@ -2,6 +2,7 @@ package com.itheima.mp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.vo.UserVO;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface IUserService extends IService<User> {
     public void deductBalance(Long id, Integer money);
 
     List<User> queryUsers(String name, Integer status, Integer minBalance, Integer maxBalance);
+
+    UserVO queryUserAndAddressById(Long id);
 }
