3d14198d8b074946e5a8e04271b90861549fb3fa
LilRind <1337476737@qq.com>
2024-05-14 12:58:54 +0800

    1. MybatisPlusInterceptor 实现mp分页插件 2. testPageQuery测试分页mp分页查询

---
+++ day01-MybatisPlus/Test/mp-demo/src/main/java/com/itheima/mp/config/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.itheima.mp.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// myBatis拦截器 实现分页插件
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 1. 创建分页插件
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        paginationInnerInterceptor.setMaxLimit(1000L); // 页数上线1000
+        // 2. 添加分页插件
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+        return interceptor;
+    }
+}
--- day01-MybatisPlus/Test/mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ day01-MybatisPlus/Test/mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -126,4 +126,6 @@ class UserMapperTest {
         userMapper.updateBalanceByIds(wrapper, amount);
     }
 
+
+
 }
